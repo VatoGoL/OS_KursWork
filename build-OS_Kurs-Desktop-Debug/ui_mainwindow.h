7b16b94ac772cdd77e6c21c81da8490ac9726a66
T1fon <danil_ignatyev@mail.ru>
2023-01-12 18:05:37 +0700

    Add Dispetcher

--- build-OS_Kurs-Desktop-Debug/ui_mainwindow.h
+++ build-OS_Kurs-Desktop-Debug/ui_mainwindow.h
@@ -14,8 +14,10 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -24,7 +26,10 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
-    QWidget *centralwidget;
+    QWidget *__wrapper;
+    QLabel *label;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -33,11 +38,21 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
         MainWindow->resize(800, 600);
-        centralwidget = new QWidget(MainWindow);
-        centralwidget->setObjectName(QStringLiteral("centralwidget"));
-        MainWindow->setCentralWidget(centralwidget);
+        __wrapper = new QWidget(MainWindow);
+        __wrapper->setObjectName(QStringLiteral("__wrapper"));
+        label = new QLabel(__wrapper);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(360, 30, 66, 20));
+        pushButton = new QPushButton(__wrapper);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(310, 230, 141, 31));
+        pushButton_2 = new QPushButton(__wrapper);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setGeometry(QRect(330, 310, 97, 36));
+        MainWindow->setCentralWidget(__wrapper);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QStringLiteral("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 30));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QStringLiteral("statusbar"));
@@ -51,6 +66,9 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        label->setText(QApplication::translate("MainWindow", "TextLabel", nullptr));
+        pushButton->setText(QApplication::translate("MainWindow", "\320\224\320\270\321\201\320\277\320\265\321\202\321\207\320\265\321\200 \320\267\320\260\320\264\320\260\321\207", nullptr));
+        pushButton_2->setText(QApplication::translate("MainWindow", "PushButton", nullptr));
     } // retranslateUi
 
 };
